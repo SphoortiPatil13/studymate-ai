41d6f3b1d0ecd77ec2fe5b558277e0c795bad92e
SphoortiPatil13 <patilsphoorti13@gmail.com>
2026-07-20 22:06:34 +0530

    solved app.jsx and login.jsx mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import Button from './components/Button';
-import Input from './components/Input';
+
 function App(){
   return (
     <>
-    <h1>Hello StudyMate AI </h1>
-    <p>This is my first react project</p>
-    <Button text="Login" color="blue" />
-    <br></br>
-    <label>Email ID</label>
-    <Input placeholder="Enter email" type="email"/> 
-    <br></br>
-    <label>Password</label>
-    <Input placeholder="Enter password" type="password"/> 
-    <br></br>
-    <Button text="Submit" color="green" />
-    
+    <Login />;
     </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import Button from './components/Button';
+import Input from './components/Input';
+function Login(){
+  return (
+    <>
+    <h1>Hello StudyMate AI </h1>
+    <p>This is my first react project</p>
+    <Button text="Login" color="blue" />
+    <br></br>
+    <label>Email ID</label>
+    <Input placeholder="Enter email" type="email"/> 
+    <br></br>
+    <label>Password</label>
+    <Input placeholder="Enter password" type="password"/> 
+    <br></br>
+    <Button text="Submit" color="green" />
+    
+    </>
+  );
+}
+export default Login;
