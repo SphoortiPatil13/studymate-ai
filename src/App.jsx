f8c59f47bd95439e150546861ebacc66f2b61c88
SphoortiPatil13 <patilsphoorti13@gmail.com>
2026-07-20 23:13:22 +0530

    Done with tailwindcss downloading and header of login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Login from './pages/Login';
-function App(){
-  return (
-    <>
-    <Login />;
-    </>
-  );
+import Login from "./pages/Login";
+
+function App() {
+  return <Login />;
 }
+
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,12 @@
-import Button from './components/Button';
-import Input from './components/Input';
-function Login(){
+function Login() {
   return (
-    <>
-    <h1>Hello StudyMate AI </h1>
-    <p>This is my first react project</p>
-    <Button text="Login" color="blue" />
-    <br></br>
-    <label>Email ID</label>
-    <Input placeholder="Enter email" type="email"/> 
-    <br></br>
-    <label>Password</label>
-    <Input placeholder="Enter password" type="password"/> 
-    <br></br>
-    <Button text="Submit" color="green" />
-    
-    </>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="bg-white rounded-2xl shadow-xl p-8 w-full max-w-md">
+       <h1 className="text-5xl text-violet-600 font-bold text-center mb-6">📚 StudyMate AI</h1>
+       <p className="text-sm text-slate-500 font-light text-center ">Your AI-powered study companion</p>
+      </div>
+    </div>
   );
 }
+
 export default Login;
