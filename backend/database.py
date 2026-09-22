b4a62e74039c2fb9e39b7492d88f316da89dd7be
SphoortiPatil13 <patilsphoorti13@gmail.com>
2026-09-22 09:24:37 +0530

    Use env variables for database connection

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,20 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+load_dotenv(ENV_PATH)
+
 
 def get_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="studymate_ai"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
     return connection
